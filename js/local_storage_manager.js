190a4eb809eab6a1ab897692d9b4f5299c96c66d
Khachig <khachKerbabian@gmail.com>
2023-03-20 21:55:08 -0500

    Code has Been Fixed of Issue #1

--- js/local_storage_manager.js
+++ js/local_storage_manager.js
@@ -41,7 +41,7 @@ LocalStorageManager.prototype.localStorageSupported = function () {
 
 // Best score getters/setters
 LocalStorageManager.prototype.getBestScore = function () {
-  return  0;
+  return this.storage.getItem(this.bestScoreKey) || 0;
 };
 
 LocalStorageManager.prototype.setBestScore = function (score) {
@@ -51,7 +51,7 @@ LocalStorageManager.prototype.setBestScore = function (score) {
 // Game state getters/setters and clearing
 LocalStorageManager.prototype.getGameState = function () {
   var stateJSON = this.storage.getItem(this.gameStateKey);
-  return  null;
+  return stateJSON ? JSON.parse(stateJSON) : null;
 };
 
 LocalStorageManager.prototype.setGameState = function (gameState) {
